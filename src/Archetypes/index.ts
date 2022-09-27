a6f5771568e2b279e6bbed1a2aa64d17dd4ba856
DevPBDias <devpbdias@gmail.com>
2022-09-27 16:39:10 -0300

    feat(req 5): criando classes herdeiras da classe Archetype

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,9 @@
 import Archetype from './Archetype';
+import Necromancer from './Necromancer';
+import Warrior from './Warrior';
+import Mage from './Mage';
+import Ranger from './Ranger';
+
+export { Warrior, Necromancer, Mage, Ranger };
 
 export default Archetype;
